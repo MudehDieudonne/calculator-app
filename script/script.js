53e4732e8c7619c9fcb0a5e77d661489c5054e86
Mk_dev_237 <mukummudeh@gmail.com>
2024-11-27 13:01:02 +0100

    js linters error v1.02

--- script/script.js
+++ script/script.js
@@ -1,28 +1,28 @@
 const display = document.querySelector('.display') // Use correct selector
 const buttons = document.querySelectorAll('button') // Select all buttons
 const spacialChars = ['%', '*', '/', '-', '+', '='] // Define special characters
-let output = ''; // Use of `let` for a mutable variable
+let output = '' // Use of `let` for a mutable variable
 
 // Define functions to calculate based on button clicks
 const calculate = (btnValue) => {
   if (btnValue === '=' && output !== '') {
     // If "%" is involved, handle percentage calculation
     output = output.includes('%')
-      ? eval(output.replace('%', '/100')) // Safely handle percentages
-      : eval(output); // Evaluate expression
+      ? eval(output.replace('%', '/100')) // handles percentages
+      : eval(output) // Evaluate expression
   } else if (btnValue === 'AC') {
-    output = ""; // Clear output
+    output = '';
   } else if (btnValue === 'Del') {
-    output = output.toString().slice(0, -1); // Delete last character
+    output = output.toString().slice(0, -1)
   } else {
     // Prevent starting with a special character
-    if (output === '' && spacialChars.includes(btnValue)) return;
-    output += btnValue; // Append the button value
+    if (output === '' && spacialChars.includes(btnValue)) return
+    output += btnValue // Append the button value
   }
-  display.value = output; // Update the display
-};
+  display.value = output // Update the display
+}
 
 // Add event listener to buttons
 buttons.forEach((button) => {
-  button.addEventListener('click', (e) => calculate(e.target.value));
-});
+  button.addEventListener('click', (e) => calculate(e.target.value))
+})
